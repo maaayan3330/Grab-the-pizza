133b9e36b6507455ed124f57a36f146cead4dc97
Maayan Ifergan <maayan3330@gmail.com>
2026-03-31 00:31:33 +0300

    add timer and start button

--- Grab the pizza/src/components/PizzaBoard.jsx
+++ Grab the pizza/src/components/PizzaBoard.jsx
@@ -1,5 +1,5 @@
 import PizzaBox from "./PizzaBox";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 export default function PizzaBoard() {
   // a state to track for every box if it is open or if it been clicked
@@ -10,6 +10,55 @@ export default function PizzaBoard() {
     { isOpen: false, isClicked: false },
     { isOpen: false, isClicked: false },
   ]);
+  // state for start button
+  const [gameStart , setGameStart] = useState(false);
+  // to aviod the same index 
+  const [lastOpenIndex, setLastOpenIndex] = useState(null);
+
+  // fucntion to handle click
+  function handleGameStart() {
+    setGameStart(true);
+  }
+
+  // function to open random box
+  function openRandomBox() {
+    // get a random index
+    const randomIndex = Math.floor(Math.random() * board.length);
+
+    // to aviod the same index 
+    while (randomIndex === lastOpenIndex) {
+      randomIndex = Math.floor(Math.random() * board.length);
+    }
+
+    // made a new arr of boxes
+    const newBoard = board.map((box , index) => {
+      return {
+        ...box ,
+        isOpen: index === randomIndex,
+        isClicked: false,
+      };
+    });
+    setBoard(newBoard);
+  }
+
+  // use effect for timmer to open every X secondes a random box
+  useEffect(() => {
+    // only if the user start the game 
+    if (!gameStart) return;
+
+    // random box on the start
+    openRandomBox();
+
+    // now every X time
+    const intervalId = setInterval(() => {
+      openRandomBox();
+    }, 750);
+
+    // clean the interval
+    return () => clearInterval(intervalId);
+  }, [gameStart]);
+
+
 
   // function to present the first line of the boxes
   function createBoxes1() {
@@ -34,6 +83,7 @@ export default function PizzaBoard() {
       <div className="flex justify-center gap-4">
         {createBoxes2()}
       </div>
+      <button onClick={handleGameStart} >START</button>
     </div>
   );
 }
